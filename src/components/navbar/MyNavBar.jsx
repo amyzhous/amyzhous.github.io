ec84c59bf22f6c44e820b4460a4f1620e03d3d34
Amy <azhou1314@gmail.com>
2020-04-18 18:27:57 -0400

    change navbar

--- src/components/navbar/MyNavBar.jsx
+++ src/components/navbar/MyNavBar.jsx
@@ -23,7 +23,7 @@ class MyNavBar extends React.Component{
                         <Col >
                             <Navbar collapseOnSelect expand="sm" sticky="top">
                                 <Link to= '/'>
-                                    <Navbar.Brand>Michael</Navbar.Brand>
+                                    <Navbar.Brand>amy zhou</Navbar.Brand>
                                 </Link>
                                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                                 <Navbar.Collapse id="responsive-navbar-nav">
